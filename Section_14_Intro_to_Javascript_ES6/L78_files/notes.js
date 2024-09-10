9747eb6120ffa73b74dd3f142df6d8cc09dd8fb8
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-10 21:13:15 +0200

    Lesson 78 Done
    This lesson looked at how to create variables in JS, how to interactively set variables in the browser and how to use the variable again in a message.

---
+++ Section_14_Intro_to_Javascript_ES6/L78_files/notes.js
@@ -0,0 +1,10 @@
+prompt("What is your name?"); //Creates a popup asking the user for their name.
+
+//creating a string variable and assigning a value:
+var myName = "Mike";
+
+// creating a variable and assigning it to the value inputted by the user in a prompt:
+var userInput = prompt("What is your name?");
+
+//outputting a variabel plus text:
+alert("Screw you"+myName+"!");
