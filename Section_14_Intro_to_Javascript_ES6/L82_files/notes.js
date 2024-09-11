c25c10d0f15e94ab7126e6de32da3d30fe33c6e9
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-11 15:13:17 +0200

    Lesson 82 completed
    This lesson covered string concatenation and using it to interact with a user through prompts and alerts.

---
+++ Section_14_Intro_to_Javascript_ES6/L82_files/notes.js
@@ -0,0 +1,11 @@
+window.alert("Hello" + "World!");
+window.alert("Hello" + " " + "World!");
+
+// Creating a greeting message:
+
+var message = "Hello";
+var userName = "";
+name = window.prompt("What is your name?");
+
+//Response:
+window.alert(message + " there, " + userName);
