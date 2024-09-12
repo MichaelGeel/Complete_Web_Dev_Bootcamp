6dce58b22a5bd09cac17314e327b9e124f0dfd75
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-12 19:59:33 +0200

    Lesson 88 Completed
    This lesson looked at the Increment and Decrement Expressions and the shorthand expressions for assigning a variable to itself +-*/ another value.

---
+++ Section_14_Intro_to_Javascript_ES6/L88_files/notes.js
@@ -0,0 +1,22 @@
+// Ordinary means of incrementation:
+var x = 5;
+x = x + 1; // x = 6
+
+// Incrementation in maths:
+// shorthard increment (increases x by 1) called Increment Expression:
+x++;
+
+// Decrement Expression (reduces a variable by 1):
+x--;
+
+// Shorthand for increasing a variable by more than 1:
+x += 3; // short for x = x + 3;
+
+// Shorthand for decreasing a variable by more than 1:
+x -= 3; // short for x = x - 3;
+
+// Shorthand for multiplying a variable by a value:
+x *= 3; // short for x = x * 3;
+
+// Shorthand for dividing a variable by a value:
+x /= 3; // short for x = x / 3;
