25f51ff805addab20b14501a1595de2d3243095f
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-12 15:19:03 +0200

    Lesson 86 Completed
    This lesson looked at the toUpperCase and toLowerCase methods for converting a string's characters all to upper case or lower case respectively.

---
+++ Section_14_Intro_to_Javascript_ES6/L85_files/notes.js
@@ -0,0 +1,11 @@
+// Creating a variable and then outputting it in all-caps using .toUpperCase().
+var myName = "Michael";
+window.alert(myName.toUpperCase());
+
+// Saving the variable in an UpperCase version.
+myName = myName.toUpperCase();
+myName
+
+// Saving the variable in a LowerCase version:
+myName = myName.toLowerCase();
+myName
