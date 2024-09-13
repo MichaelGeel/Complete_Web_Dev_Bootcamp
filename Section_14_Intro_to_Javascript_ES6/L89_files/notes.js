85d1b5fd65d6e081061a6ca4fffa2f200449d93c
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-13 16:00:20 +0200

    Lesson 89 Completed
    Learnt how to create and execute a function.

---
+++ Section_14_Intro_to_Javascript_ES6/L89_files/notes.js
@@ -0,0 +1,13 @@
+// function template:
+function funcName() {
+    //insert code here
+}
+
+// executing a functioN;
+funcName();
+
+// creating and executing a simple function:
+function getMilk() {
+    window.alert("Got Milk.");
+}
+getMilk();
