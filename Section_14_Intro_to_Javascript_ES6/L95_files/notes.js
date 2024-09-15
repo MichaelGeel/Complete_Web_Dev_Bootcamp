03985bc5a515f3248d39fc45a0443d5ecf0806e3
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-15 15:36:22 +0200

    Lesson 95 Completed
    This lesson covered how to create and work with functions that return an output.

---
+++ Section_14_Intro_to_Javascript_ES6/L95_files/notes.js
@@ -0,0 +1,11 @@
+// creating a function that returns an output:
+function funcName(input){
+    //code here;
+    return returnValue;
+}
+
+// assigning a function call output to a variable:
+var funcOutput = funcName(input);
+
+// Creating a multi-input function:
+function funcName(input1, input2){}
