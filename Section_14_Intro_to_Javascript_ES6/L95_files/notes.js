2f3018d75bf3ff880730a0a1fe22153524328f70
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-15 15:42:24 +0200

    Lesson 96 Completed
    This lesson introduced the next coding challenge, which is to create a BMI calculator function that takes 2 inputs and returns an output.

--- Section_14_Intro_to_Javascript_ES6/L95_files/notes.js
+++ Section_14_Intro_to_Javascript_ES6/L95_files/notes.js
@@ -9,3 +9,6 @@ var funcOutput = funcName(input);
 
 // Creating a multi-input function:
 function funcName(input1, input2){}
+
+// Calling a multi-input function by referring to the inputs by name:
+funcName(input1=5, input2=10);
