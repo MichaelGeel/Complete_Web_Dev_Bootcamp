3cdc594265a2f0c88310be78ac2d873c5964df73
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-16 19:34:16 +0200

    Lesson 99 Completed
    This lesson looked at the random function in javascript, how it behaves and how to manipulate the outputs to suit any random number generation needs.

---
+++ Section_15_Intermediate_JavaScript/L99_files/notes.js
@@ -0,0 +1,16 @@
+// Generating a random number and assigning to a variable:
+var n = Math.random();
+
+// Simulating a dice roll:
+var n = Math.random();
+n *= 6;
+n = Math.floor(n)+1;
+n
+
+// Challenge solution:
+var userName = window.prompt("Enter your name: ");
+var crushName = window.prompt("Now enter your crushes name: ");
+var n = Math.random();
+n *= 101;
+n = Math.floor(n);
+window.alert("The love score between " + userName + " and " + crushName + " is: " + n + "%.");
