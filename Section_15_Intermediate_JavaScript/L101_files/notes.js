e7c076014b6dd10df215d8e3e5d8abbeb5c5a396
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-16 22:08:10 +0200

    Lesson 101 Completed
    This lesson covered the different Comparatives used for testing equality and made special mention of the difference between the === and == comparatives.

---
+++ Section_15_Intermediate_JavaScript/L101_files/notes.js
@@ -0,0 +1,17 @@
+// === vs ==
+var a = 1; //a number
+var b = "1"; //a string
+
+// Using ===
+if (a === b){
+    console.log("Yes");
+} else {
+    console.log("No");
+} // This conditional will output No to the console.
+
+// Using ==
+if (a == b){
+    console.log("Yes");
+} else {
+    console.log("No");
+} // This conditional will output Yes to the console.
