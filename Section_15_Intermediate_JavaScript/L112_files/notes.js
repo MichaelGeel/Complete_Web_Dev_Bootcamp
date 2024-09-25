4c71655a3794fda31f587504a8434c45cb323f7f
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-25 13:13:04 +0200

    Lesson 112 and 113 Completed
    Lesson 112 covers the solution of the Fibonacci Sequence function, using a for loop in comparison to the while loop used initially. Lesson 113 is just a section closing lesson.

--- Section_15_Intermediate_JavaScript/L112_files/notes.js
+++ Section_15_Intermediate_JavaScript/L112_files/notes.js
@@ -17,3 +17,19 @@ else {
 }
 return output;
 
+// Course fibonacci sequence solution:
+var output = []
+    
+if (n === 1) {
+    output = [0];
+}
+else if (n === 2){
+    output = [0, 1];
+}
+else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++){
+        output.push(output[output.length-1] + output[output.length-2]); // Adding third item to output
+    }
+}
+return output;
