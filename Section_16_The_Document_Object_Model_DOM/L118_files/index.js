53f277528de541ba297ed0b50bb24f5b55a75fd9
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-26 20:15:13 +0200

    Lesson 118 Completed
    This lesson looked at some key points regarding manipulating the styling of HTML elements using JavaScript.

---
+++ Section_16_The_Document_Object_Model_DOM/L118_files/index.js
@@ -0,0 +1,11 @@
+// Changing the font color styling of the h1 element:
+document.querySelector("h1").style.color = "red";
+
+// Changing the font size using the JavaScrip property name:
+document.querySelector("h1").style.fontSize = "3rem";
+
+// Changing the padding property of the h1 element (notice it's a number in a string):
+document.querySelector("h1").style.padding = "20px 10px";
+
+// Lesson Challenge solution (mine):
+document.querySelector("button").style.backgroundColor = "yellow";
