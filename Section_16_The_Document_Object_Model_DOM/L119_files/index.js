41c2b5868bc31f4901d82ed9b925c3b6eee1e8f1
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-27 15:06:45 +0200

    Lesson 119 Completed
    This lesson covered the rule of The Separation of Concerns and how to use the toggling of classes on elements in the DOM to maintain good practice with the Rule.

---
+++ Section_16_The_Document_Object_Model_DOM/L119_files/index.js
@@ -0,0 +1,14 @@
+// Getting the classList property of the button:
+document.querySelector("button").classList;
+
+// Adding a class "invisible" to the button element:
+document.querySelector("button").classList.add("invisible");
+
+// Removing the "invisible" class from the button element:
+document.querySelector("button").classList.remove("invisible");
+
+// Toggling the "invisible" class on the button element:
+document.querySelector("button").classList.toggle("invisible");
+
+// Lesson Challenge - Toggling the huge class on the h1 element:
+document.querySelector("h1").classList.toggle("huge");
