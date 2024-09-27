b3a08e5e5ce44a9aca69d4e36b3e0f67ee5d3e9b
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-09-27 15:46:49 +0200

    Lesson 121 and 122, and Section 16 Completed
    Lesson 121 covered how to view, get and set attributes of HTML elements using JavaScript. Lesson 122 is a closing video to mark the end of Section 16.

---
+++ Section_16_The_Document_Object_Model_DOM/L121_files/index.js
@@ -0,0 +1,8 @@
+// Getting a list of attributes attached to a selected HTML element:
+document.querySelector("a").attributes;
+
+// Getting the value of a specified attribute attached to a selected element:
+document.querySelector("a").getAttribute("href");
+
+// Setting the value of a specific attribute on to a selected element:
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
