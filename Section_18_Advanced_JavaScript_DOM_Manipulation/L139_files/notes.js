f8f3c45e409b85beb2f52ecf09266a4c37ab9741
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-01 22:23:17 +0200

    Lesson 139 Completed
    This lesson looked at how to play an audio file as well as the this keyword that refers to the object that triggered a click event.

---
+++ Section_18_Advanced_JavaScript_DOM_Manipulation/L139_files/notes.js
@@ -0,0 +1,8 @@
+// Using JavaScript to play a sound file:
+var audio = new Audio("audio_file.mp3");
+audio.play();
+
+// Referring to the identitiy of the object that triggered an event and print it:
+console.log(this);
+
+// 
--- Section_18_Advanced_JavaScript_DOM_Manipulation/index.js
+++ Section_18_Advanced_JavaScript_DOM_Manipulation/index.js
@@ -9,10 +9,13 @@
 //     window.alert("A button was clicked.");
 // });
 
-// Lesson Challenge: Adding an event listener efficiently to each button:
+// Lesson 137 Challenge: Adding an event listener efficiently to each button:
 for(var i = 0; i < document.querySelectorAll("button").length; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    window.alert("A button was clicked.");
-});
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        // var audio = new Audio("./assets/sounds/tom-1.mp3");
+        // audio.play();
+        // console.log(this);
+        this.style.color = 'white';
+    });
 }
 
