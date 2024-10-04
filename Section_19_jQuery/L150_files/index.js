bd4bbf769c902fbfe2234916e0f55307387c4d75
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-04 20:51:45 +0200

    Lesson 150 Completed
    This lesson looked at how to incorporate the jQuery library into JavaScript as well as some considerations to make when importing jQuery via CDN script

---
+++ Section_19_jQuery/L150_files/index.js
@@ -0,0 +1,7 @@
+// Changed the h1 color to red:
+// $("h1").css("color", "red");
+
+// Checking if the jQuery library is loaded and ready before executing a callback function containing the code to run:
+$(document).ready(function(){
+    $("h1").css("color", "red");
+});
