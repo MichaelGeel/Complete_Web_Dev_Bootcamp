810fbfaa13dba1369721dda3d87ea834c3b1fc23
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-04 22:06:01 +0200

    Lesson 153 Completed
    This lesson covered adding and removing single/multiple classes from an element with jQuery as well as how to check for the presence of a class on an element with jQuery.

--- Section_19_jQuery/index.js
+++ Section_19_jQuery/index.js
@@ -2,6 +2,23 @@
 // $("h1").css("color", "red");
 
 // Checking if the jQuery library is loaded and ready before executing a callback function containing the code to run:
-$(document).ready(function(){
-    $("h1#header").css("color", "red");
-});
+// $(document).ready(function(){
+//     $("h1#header").css("color", "red");
+// });
+
+// Adding a class to an element using jQuery:
+$("h1").addClass("big-title");
+
+// Removing a class from an element:
+$("h1").removeClass("big-title");
+
+// Adding multiple classes to an element:
+$("h1").addClass("big-title margin-50");
+
+// Removing multiple classes from an element:
+$("h1").removeClass("big-title margin-50");
+
+// Checking for a class on an elemetn:
+$("h1").hasClass("margin-50"); // returns a boolean true/false
+
+// 
