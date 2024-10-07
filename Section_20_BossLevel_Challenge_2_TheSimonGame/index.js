11b479a998088e7c824d466899396114f8bb6e39
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-07 20:58:33 +0200

    Game Logic continued.
    Implemented a random sequence generator based on level, working on the sequence display function now.

--- Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
+++ Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
@@ -1,6 +1,24 @@
+function createSequence(length){
+    var blockSequence = [];
+    for(var i=0; i<length;i++){
+        blockSequence.push(Math.floor((Math.random*4)+1));
+    }
+    return blockSequence;
+}
 
+function showSequence(blockSeq){
+    for(var i=0;i<blockSeq.length;i++){
+        
+    }
+}
 
-
+function gameOn(){
+    var levelCount = 1;
+    var gameOver = false;
+    while(!gameOver) {
+        sequence = createSequence(levelCount);
+    }
+}
 
 // This starts the game.
-$(document).click()
+$(document).click();
