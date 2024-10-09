d6fd225ea6e1d6177110248d74ea28e6c8146324
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-09 20:41:56 +0200

    Block press function progress
    The function to show which blocks are in the sequence has been created, but shows all the blocks in the sequence at the same time. Will be diverting to the instructions for this challenge here.

--- Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
+++ Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
@@ -10,16 +10,27 @@ function createSequence(length){ // Verified with unit testing.
 function buttonToggle(btnColor){  // Verified with unit testing.
     setTimeout(function(){
         $(btnColor).addClass("pressed");
-    }, 500);
+    }, 50);
     setTimeout(function(){
-
         $(btnColor).removeClass("pressed");
-    }, 1000);
+    }, 100);
 }
 
 function showSequence(blockSeq){
     for(var i=0;i<blockSeq.length;i++){
-        
+        switch(blockSeq[i]){
+            case 1:
+                buttonToggle(".green");
+                break;
+            case 2:
+                buttonToggle(".red");
+                break;
+            case 3:
+                buttonToggle(".yellow");
+                break;
+            default:
+                buttonToggle(".blue");
+        }
     }
 }
 
@@ -27,11 +38,12 @@ function gameOn(){
     var levelCount = 1;
     var gameOver = false;
     // while(!gameOver) {
-    sequence = createSequence(levelCount);
     levelCount++;
     sequence = createSequence(levelCount);
+    showSequence(sequence);
     // }
 }
 
 // This starts the game.
 $(document).keypress(gameOn); // Verified with testing.
+buttonToggle("green");
