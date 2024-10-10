dec7be3bf6967fd5706cc042387ef1754390bc60
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-10 20:06:38 +0200

    Simon Game Progress
    Restarted Development. Tested the finished product and the expectation was different to the progress I'd made. Have implemented and unit tested the following successfully: gameover functionality, user button press visual functionality (minus audio queues) and to check if a particular button press is correct in accordance to the generated list (if empty, defaults to a game over sequence).

--- Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
+++ Section_20_BossLevel_Challenge_2_TheSimonGame/index.js
@@ -1,49 +1,55 @@
-function createSequence(length){ // Verified with unit testing.
-    var blockSequence = [];
-    for(var i=0; i<length;i++){
-        blockSequence.push(Math.floor((Math.random()*4)+1));
-    }
-    console.log(blockSequence);
-    return blockSequence;
+// Global Vars:
+var generatedList = [];
+var userList = [];
+var level = 1;
+
+
+function gameOver(){
+    $("h1").text("Game Over, Press Any Key to Restart");
+    $("body").toggleClass("game-over");
+    setTimeout(function(){
+        $("body").toggleClass("game-over");
+    }, 200);
+    generatedList = [];
+    userList = [];
+    level = 1;
 }
 
-function buttonToggle(btnColor){  // Verified with unit testing.
+function checkInput(){
+    if(userList[userList.length-1]!==generatedList[userList.length-1]){
+        gameOver();
+    }
+}
+
+$(".btn").click(function(event){
+    var color = event.target.classList[1];
+    userList.push(color)
+    $("."+color).toggleClass("pressed");
     setTimeout(function(){
-        $(btnColor).addClass("pressed");
-    }, 50);
-    setTimeout(function(){
-        $(btnColor).removeClass("pressed");
+        $("."+color).toggleClass("pressed");
     }, 100);
-}
+    checkInput();
+});
 
-function showSequence(blockSeq){
-    for(var i=0;i<blockSeq.length;i++){
-        switch(blockSeq[i]){
-            case 1:
-                buttonToggle(".green");
-                break;
-            case 2:
-                buttonToggle(".red");
-                break;
-            case 3:
-                buttonToggle(".yellow");
-                break;
-            default:
-                buttonToggle(".blue");
-        }
-    }
-}
+// function createSequence(length){ // Verified with unit testing.
+//     var blockSequence = [];
+//     for(var i=0; i<length;i++){
+//         blockSequence.push(Math.floor((Math.random()*4)+1));
+//     }
+//     console.log(blockSequence);
+//     return blockSequence;
+// }
 
-function gameOn(){
-    var levelCount = 1;
-    var gameOver = false;
-    // while(!gameOver) {
-    levelCount++;
-    sequence = createSequence(levelCount);
-    showSequence(sequence);
-    // }
-}
+// function gameOn(){
+//     var levelCount = 1;
+//     var gameOver = false;
+//     // while(!gameOver) {
+//     levelCount++;
+//     sequence = createSequence(levelCount);
+//     showSequence(sequence);
+//     // }
+// }
 
-// This starts the game.
-$(document).keypress(gameOn); // Verified with testing.
-buttonToggle("green");
+// // This starts the game.
+// $(document).keypress(gameOn); // Verified with testing.
+// buttonToggle("green");
