2d36c65c2da774155a7a038dd5e893eb6354f6e3
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-10-18 20:13:27 +0200

    Lesson 197 Completed
    This lesson covered how to initialize a node project, install packages using npm and how to import from packages using ECMAScript Modules.

---
+++ Section_23_NodeJS/L197_files/index.js
@@ -0,0 +1,12 @@
+// let generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// let sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import randomSuperhero from "superheroes";
+
+
+let superHeroName = randomSuperhero();
+console.log(`I am ${superHeroName}!`);
